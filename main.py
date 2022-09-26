e98f7ad8036056d96fb70829ab1158e0e99669ac
Mahesh Maan <mahesh.maan@greyb.com>
2022-09-26 15:05:47 +0530

    Simplify code, architecture, and tests

--- main.py
+++ main.py
@@ -5,74 +5,92 @@ Attributes:
     PORT (int): Port number
 """
 
-
 import os
 import uvicorn
+import json
+import re
 from fastapi import FastAPI, Response
-
 import dotenv
+import boto3
+import botocore
+
 dotenv.load_dotenv()
 
-#pylint: disable=wrong-import-position
-from core import crud
+from core.storage import S3Bucket
 
-PORT = int(os.environ['PORT'])
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+config = botocore.config.Config(
+    read_timeout=400,
+    connect_timeout=400,
+    retries={"max_attempts": 0}
+)
+credentials = {
+    'aws_access_key_id': AWS_ACCESS_KEY_ID,
+    'aws_secret_access_key': AWS_SECRET_ACCESS_KEY
+}
+botoclient = boto3.client('s3', **credentials, config=config)
+bucket_name = os.environ.get("AWS_S3_BUCKET_NAME")
+s3_storage = S3Bucket(botoclient, bucket_name)
 
 app = FastAPI()
 
+def get_drawing_prefix(doc_id):
+    """Maps document ids to their drawing path prefixes
+    For US Patent No. 7,654,321 the drawings are stored as follows:
+        07654321-1.tif
+        07654321-2.tif
+        ...
+        ...
+    For US patent applications, say, for US20080156487A1, they're stored as:
+        US20080156487A1-1.tif
+        US20080156487A1-2.tif
+        ...
+        ...
+    This function creates the appropriate key prefix on the basis of whether
+    the supplied number is a patent or an application.
+    Args:
+        doc_id (str): Document identifier (e.g. patent number)
+    Returns:
+        str: Drawing prefix
+    """
+    if len(doc_id) > 12:
+        return f'images/{doc_id}-'
+
+    num = re.search(r'\d+', doc_id).group(0)
+    while len(num) < 8:
+        num = '0' + num
+    return f'images/{num}-'
+
+
 @app.get('/patents/{doc_id}')
-@app.get('/docs/{doc_id}')
 async def get_doc(doc_id):
     """Return a document's data in JSON format
-
-    Args:
-        doc_id (str): Document identifier (e.g. patent number)
-
-    Returns:
-        dict: Document data
     """
-    return crud.get_doc(doc_id)
-
-@app.delete('/patents/{doc_id}')
-@app.delete('/docs/{doc_id}')
-async def delete_doc(doc_id):
-    """Delete the document
-
-    Args:
-        doc_id (str): Document identifier (e.g. patent number)
-
-    Returns:
-       None
-    """
-    crud.delete_doc(doc_id)
+    doc = s3_storage.get(f"patents/{doc_id}.json")
+    return json.loads(doc)
 
 @app.get('/patents/{doc_id}/drawings')
-@app.get('/docs/{doc_id}/drawings')
 async def list_drawings(doc_id):
-    """Return a list of drawings associated with a document (patent).
-
-    Args:
-        doc_id (str): Document identifier (e.g. patent number)
-
-    Returns:
-        list: Drawing identifiers, e.g. ["1", "2", "3"]
+    """Return a list of drawings associated with a document, e.g., [1, 2, 3]
     """
-    return crud.list_drawings(doc_id)
+    key_prefix = get_drawing_prefix(doc_id)
+    keys = s3_storage.ls(key_prefix)
+    drawings = [re.search(r'-(\d+)', key).group(1) for key in keys]
+    return drawings
 
 @app.get('/patents/{doc_id}/drawings/{drawing_num}')
-@app.get('/docs/{doc_id}/drawings/{drawing_num}')
 async def get_drawing(doc_id, drawing_num):
     """Return image data of a particular drawing
-
-    Args:
-        doc_id (str): Drawing identifier (e.g. patent number)
-        drawing_num (str): Drawing number, e.g., "1"
-
-    Returns:
-        bytes: Image data
     """
-    image = crud.get_drawing(doc_id, drawing_num)
-    return Response(content=image, media_type='image/tiff')
+    key_prefix = get_drawing_prefix(doc_id)
+    key = f'{key_prefix}{drawing_num}.tif'
+    print(key)
+    tif_data = s3_storage.get(key)
+    return Response(content=tif_data, media_type='image/tiff')
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+    port = int(os.environ["PORT"])
+    uvicorn.run(app, host="0.0.0.0", port=port)
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,13 +1,12 @@
-"""
-Test all server routes
-"""
 import unittest
 import os
 import socket
 import requests
-import testutil
+from pathlib import Path
+import dotenv
 
-testutil.load_test_environment()
+BASE_DIR = str(Path(__file__).parent.parent.resolve())
+dotenv.load_dotenv(f"{BASE_DIR}/.env")
 
 PROTOCOL = 'http'
 HOST = '127.0.0.1'
@@ -21,56 +20,28 @@ if server_not_running:
 @unittest.skipIf(server_not_running, 'Works only when true')
 class TestServer(unittest.TestCase):
 
-    """Test the REST API routes
-    """
-
-    def test_document_route(self):
-        """Can retrieve a document's data
-        """
-        response = self.call_route('/docs/US7654321B2')
+    def test__document_route(self):
+        response = self.call_route('/patents/US7654321B2')
         self.assertEqual(200, response.status_code)
         patent = response.json()
         self.assertIsInstance(patent, dict)
         self.assertEqual('US7654321B2', patent['publicationNumber'])
 
-    def test_drawing_listing_route(self):
-        """Can retrieve a list of drawings associated with a document
-        """
-        response = self.call_route('/docs/US7654321B2/drawings')
+    def test__drawing_listing_route(self):
+        response = self.call_route('/patents/US7654321B2/drawings')
         self.assertEqual(200, response.status_code)
         drawings = response.json()
         self.assertIsInstance(drawings, list)
         self.assertEqual(8, len(drawings))
 
-    def test_drawing_route(self):
-        """Can obtain a drawing
-        """
-        response = self.call_route('/docs/US7654321B2/drawings/1')
+    def test__drawing_route(self):
+        response = self.call_route('/patents/US7654321B2/drawings/1')
         self.assertEqual(200, response.status_code)
-
-    def test_delete_route(self):
-        """ Can Delete File
-        """
-        pn = 'US7654321A2'
-        route = f'/docs/{pn}'
-
-        response = self.call_route(route, 'delete')
-        self.assertEqual(200, response.status_code)
-
-        response = self.call_route(route, 'delete')
-        self.assertEqual(404, response.status_code)
 
     @staticmethod
     def call_route(route, method='get'):
         """Make a GET request to a specific route
-
-        Args:
-            route (str): Target route
-
-        Returns:
-            Response: Response object from `requests` module
         """
-
         url = f'{PROTOCOL}://{HOST}:{PORT}' + route
         response = getattr(requests, method)(url)
         return response
