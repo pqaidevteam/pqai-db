a3792937553f770bc06153aeeae92bd2072ab051
Raghav Arora <ragaro214@gmail.com>
2022-10-06 22:56:27 -0400

    added test scripts to manipulate .tiff images; updated requirements.txt with opencv

--- core/crud.py
+++ core/crud.py
@@ -10,6 +10,8 @@ storage or multiple storages (e.g. separate for text and image data, etc.)
 import os
 import json
 import re
+import cv2
+import numpy as np
 from core.s3wrapper import S3Bucket
 from core.local_storage_wrapper import LocalStorage
 from core.mongo_wrapper import Mongo
@@ -47,6 +49,7 @@ def get_doc(doc_id):
         case _: #default
             pass
 
+
 def delete_doc(doc_id):
     """Delete a document
 
@@ -69,6 +72,7 @@ def delete_doc(doc_id):
         case _: # default
             pass
 
+
 def list_drawings(doc_id):
     """Summary
 
@@ -82,6 +86,7 @@ def list_drawings(doc_id):
     keys = S3_BUCKET_DRAWINGS.list(key_prefix)
     drawings = [re.search(r'-(\d+)', key).group(1) for key in keys]
     return drawings
+
 
 def get_drawing(doc_id, drawing_num):
     """Summary
@@ -97,6 +102,7 @@ def get_drawing(doc_id, drawing_num):
     key = f'{key_prefix}{drawing_num}.tif'
     tif_data = S3_BUCKET_DRAWINGS.get(key)
     return tif_data
+
 
 def _drawing_prefix(doc_id):
     """Maps document ids to their drawing path prefixes
@@ -127,3 +133,34 @@ def _drawing_prefix(doc_id):
     while len(num) < 8:
         num = '0' + num
     return f'images/{num}-'
+
+
+
+def _downscale_drawing(image):
+    """Downscale the given image.
+    
+    Args:
+        image (numpy.arra): Representation of image as a numpy array
+    """
+    w, h = 0, 0
+    im = cv2.resize(im, (w, h), interpolation=cv2.INTER_AREA)
+
+def get_patent_thumbnail(doc_id, thumbnail_num):
+    """Returns the desired thumbnail corresponding to the specified document.
+
+    Args:
+        doc_id (str): Document identifier (e.g. patent number)
+        thumbnail_num (str): Thumbnail number, e.g. "2"
+
+    Returns:
+        bytes: Image data (of the thumbnail)
+    """
+    key_prefix = _drawing_prefix(doc_id)
+    key = f'{key_prefix}{thumbnail_num}.tif'
+    tif_data = S3_BUCKET_DRAWINGS.get(key)
+    im = cv2.imread(tif_data)
+    _downscale_drawing(im)
+    img_encode = cv2.imencode('.tiff', im)[1]
+    data_encode = np.array(img_encode)
+    byte_encode = data_encode.tobytes()
+    return byte_encode
